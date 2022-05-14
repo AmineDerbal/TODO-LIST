8c6840d3332831c3d212d66a669bb68384441470
Kyu931 <aminedoran31@gmail.com>
2022-05-14 14:37:22 +0100

    update modal add tasks

--- src/header.js
+++ src/header.js
@@ -1,8 +1,11 @@
-
+const modal = document.querySelector(".modal");
 const header = () =>{
   const headerTag = document.createElement('header');
   headerTag.classList.add('header');
   document.body.appendChild(headerTag);
+
+  const div = document.createElement('div');
+  headerTag.appendChild(div);
 
   const headerTitle = document.createElement('h1');
   headerTitle.classList.add('header-title');
@@ -12,15 +15,25 @@ const header = () =>{
   const headerTask = document.createElement('div');
   headerTask.classList.add('header-task');
   headerTag.appendChild(headerTask);
+   
+  const headerButton = document.createElement('button');
+  headerButton.classList.add('button');
+  headerTask.appendChild(headerButton);
+  
+  headerButton.innerHTML='<i class="bi bi-plus-square"></i>  add Task';
+ 
 
-  const addTask = document.createElement('i');
-  addTask.className='bi bi-plus-square';
-  headerTask.textContent = "add Task";
-  headerTask.appendChild(addTask);
-
-  headerTask.addEventListener('click',()=>{
+  headerButton.addEventListener('click',()=>{
+    
+    modal.style.display='block';
     
   })
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
 }
 
