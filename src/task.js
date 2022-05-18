501c477f627a83454ee832df80d6d704facc3514
Kyu931 <aminedoran31@gmail.com>
2022-05-18 16:48:40 +0100

    rename todo.js to task.js

---
+++ src/task.js
@@ -0,0 +1,11 @@
+export class Task {
+  constructor(title,description,dueDate,priority,project){
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.project = project;
+  }
+}
+
+
