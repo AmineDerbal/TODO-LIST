dea18b1d19af40c26cc816eb779ca4738c83618a
Kyu931 <aminedoran31@gmail.com>
2022-05-19 11:42:37 +0100

    add cancel function to the modal task

--- src/task.js
+++ src/task.js
@@ -1,4 +1,4 @@
-export class Task {
+ class Task {
   constructor(title,description,dueDate,priority,project){
     this.title = title;
     this.description = description;
@@ -7,5 +7,28 @@ export class Task {
     this.project = project;
   }
 }
+
+const addTask = document.getElementById('addTask');
+addTask.addEventListener('click',(e)=>{
+  let task = new Task(
+    document.getElementById("taskName").value,
+    document.getElementById("taskDesc").value,
+    document.getElementById("appt").value,
+    document.getElementById("priority").value,
+    document.getElementById("project").value
+  );
+  console.log(task.title);
+  console.log(task.description);
+  console.log(task.dueDate);
+  console.log(task.priority);
+  console.log(task.project);
+})
+
+const cancelModalTask = document.getElementById("cancelform");
+cancelModalTask.addEventListener("click",()=>{
+  const modal = document.querySelector(".modal");
+  modal.style.display="none";
+})
+
 
 
