e179e7425864448b5ed0a8e3df9715be2bc607c6
Kyu931 <aminedoran31@gmail.com>
2022-05-21 19:31:34 +0100

    update imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import "./style.css"
 import 'bootstrap-icons/font/bootstrap-icons.css'
-//import Todo from './todo.js';
+import {Task,displayAllTasks} from './task.js';
 import header from './header.js';
 import mainContent from './mainContent.js';
 
 header();
 mainContent();
+displayAllTasks();
+
+
+
+
+
 
 
