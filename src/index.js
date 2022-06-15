7138eab7e1780b43f57a7f3bb007a3b852f8355f
Kyu931 <aminedoran31@gmail.com>
2022-06-15 21:33:11 +0100

    moving window event from header  to index file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import "./style.css"
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import {Task,displayAllTasks} from './task.js';
-import header from './header.js';
-import mainContent from './mainContent.js';
+import "./style.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import {  displayAllTasks } from "./task.js";
+import header from "./header.js";
+import mainContent from "./mainContent.js";
 
 header();
 mainContent();
 displayAllTasks();
 
+const taskModal = document.querySelector(".myModal");
+const projectModal = document.querySelector(".projectModal");
 
-
-
-
-
-
+window.onclick = function (e) {
+  if (e.target == taskModal) {
+    taskModal.style.display = "none";
+  } else if (e.target == projectModal) {
+    projectModal.style.display = "none";
+  }
+};
