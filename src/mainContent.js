9232c6191bdfd358f78c4de65bc606485a8b1582
Kyu931 <aminedoran31@gmail.com>
2022-06-20 16:49:01 +0100

    add display project funtion

--- src/mainContent.js
+++ src/mainContent.js
@@ -1,43 +1,55 @@
 import { newProjectModal } from "./modal";
+import { displayProject, hideAllDropDownElements } from "./render";
 
-const mainContent = ()=>{
- 
-const main = document.createElement('div');
-main.classList.add('main');
-document.body.appendChild(main);
+const mainContent = () => {
+  const main = document.createElement("div");
+  main.classList.add("main");
+  document.body.appendChild(main);
 
-newProjectModal();
+  //Create a modal to add new project
+  newProjectModal();
 
-const sidebar = document.createElement('nav');
-sidebar.classList.add('sidebar');
-main.appendChild(sidebar);
+  // create a sidebar
+  const sidebar = document.createElement("nav");
+  sidebar.classList.add("sidebar");
+  main.appendChild(sidebar);
 
-const newProject = document.createElement('button');
-newProject.classList.add('button');
-newProject.id="newProject";
-newProject.innerHTML = '<i class="bi bi-plus-lg"></i>  New Project';
-sidebar.appendChild(newProject);
+  const newProject = document.createElement("button");
+  newProject.classList.add("button");
+  newProject.id = "newProject";
+  newProject.innerHTML = '<i class="bi bi-plus-lg"></i>  New Project';
+  sidebar.appendChild(newProject);
 
-  const content = document.createElement('div');
-content.classList.add('content');
-main.appendChild(content);
+  const modal = document.querySelector(".projectModal");
 
-const project = document.createElement('div');
-project.classList.add('project');
-sidebar.appendChild(project);
+  newProject.addEventListener("click", () => {
+    hideAllDropDownElements();
+    if (modal.style.display == "block") {
+      modal.style.display = "none";
+    } else {
+      modal.style.display = "block";
+    }
+  });
 
-const allTasks = document.createElement('a');
-allTasks.classList.add('all-tasks');
-allTasks.textContent = "All TASKS";
-project.appendChild(allTasks);
+  const project = document.createElement("div");
+  project.classList.add("project");
+  sidebar.appendChild(project);
 
-const defaultProject = document.createElement('a');
-defaultProject.classList.add('default-project');
-defaultProject.textContent = "Default Project";
-project.appendChild(defaultProject);
+  if (
+    !localStorage.getItem("projectsList") ||
+    JSON.parse(localStorage.getItem("projectsList")).length == 0
+  ) {
+    let projectsList = [];
+    projectsList.push("default-project");
+    localStorage.setItem("projectsList", JSON.stringify(projectsList));
+  }
 
+  displayProject();
 
-}
+  //Create a content section where todo lists whill be displayed
+  const content = document.createElement("div");
+  content.classList.add("content");
+  main.appendChild(content);
+};
 
-
-export default mainContent
+export default mainContent;
