aef11b9ee257c696529df987ffeb5b86c0c862b1
Kyu931 <aminedoran31@gmail.com>
2022-06-25 10:45:16 +0100

    delete console log

--- src/task.js
+++ src/task.js
@@ -1,7 +1,7 @@
- import {renderAllTasks} from './render.js';
- 
- class Task {
-  constructor(title,description,dueDate,priority,project,completed){
+import { renderAllTasks } from "./render.js";
+
+class Task {
+  constructor(title, description, dueDate, priority, project, completed) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
@@ -11,12 +11,10 @@
   }
 }
 
- let tasksList = [] ;
+let tasksList = [];
 
-
-const addTask = document.getElementById('addTask');
-addTask.addEventListener('click',()=>{
-
+const addTask = document.getElementById("addTask");
+addTask.addEventListener("click", () => {
   let task = new Task(
     document.getElementById("taskName").value,
     document.getElementById("taskDesc").value,
@@ -25,49 +23,30 @@ addTask.addEventListener('click',()=>{
     document.getElementById("project").value
   );
   tasksList.push(task);
-  localStorage.setItem("tasks",JSON.stringify(tasksList));
+  localStorage.setItem("tasks", JSON.stringify(tasksList));
   cancelModalTask();
 
-  console.log(task.title);
-  console.log(task.description);
-  console.log(task.dueDate);
-  console.log(task.priority);
-  console.log(task.project);
   displayAllTasks();
 });
 
 const cancelModalTask = () => {
   const modal = document.querySelector(".modal");
-  modal.style.display="none";
-} 
+  modal.style.display = "none";
+};
 
 const cancel = document.getElementById("cancelform");
-cancel.addEventListener("click",()=>{
-cancelModalTask();
-  
-})
-
+cancel.addEventListener("click", () => {
+  cancelModalTask();
+});
 
 export const displayAllTasks = () => {
- 
+  if (!localStorage.getItem("tasks")) {
+    tasksList = [];
+    return;
+  }
 
-if (!localStorage.getItem('tasks')){
-  tasksList = [];
-  return;
-} 
+  tasksList = JSON.parse(localStorage.getItem("tasks"));
+  console.log(tasksList);
 
-tasksList = JSON.parse(localStorage.getItem("tasks"));
-console.log(tasksList);
-
-renderAllTasks(tasksList);
- 
-
-
-
-
-
-
-}
-
-
-
+  renderAllTasks(tasksList);
+};
